a32873aaefc8141af8a70d47b1eb10b564596322
Rufo Severino <rufoseverino@admins-MacBook-Pro-3.local>
2016-07-22 16:39:31 +0100

    player name

--- src/test/scala/PlayerTest.scala
+++ src/test/scala/PlayerTest.scala
@@ -6,8 +6,9 @@ import org.scalatest.{FeatureSpec, FlatSpec, Matchers}
 
 class PlayerTest extends FlatSpec with Matchers{
   "Player" should "be instanciated" in {
-    Player() shouldBe a [Player]
+    Player("RR", 2) shouldBe a [Player]
   }
+
 
 
 }
