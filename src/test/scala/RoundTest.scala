824c2ecd92c234c35fc7bf74336d59955c44f086
David Navalho <davidnavalho@jinxMac.local>
2016-07-22 15:14:04 +0100

    Push for our devops

--- src/test/scala/RoundTest.scala
+++ src/test/scala/RoundTest.scala
@@ -1,6 +1,12 @@
+import org.scalatest.{FlatSpec, Matchers}
+
 /**
   * Created by davidnavalho on 22/07/2016.
   */
-class RoundTest {
+class RoundTest extends FlatSpec with Matchers{
 
+  "Round" should "be instantiated" in {
+    Round() shouldBe a [Round]
+    
+  }
 }
