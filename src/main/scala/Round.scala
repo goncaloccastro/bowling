bf99ca46405620610e3d136a81b0cd534e9ad8a3
David Navalho <davidnavalho@jinxMac.local>
2016-07-22 15:19:53 +0100

    last commit for presentation

--- src/main/scala/Round.scala
+++ src/main/scala/Round.scala
@@ -1,6 +1,6 @@
 /**
   * Created by davidnavalho on 22/07/2016.
   */
-class Round {
+case class Round() {
 
 }
--- src/test/scala/RoundTest.scala
+++ src/test/scala/RoundTest.scala
@@ -7,6 +7,10 @@ class RoundTest extends FlatSpec with Matchers{
 
   "Round" should "be instantiated" in {
     Round() shouldBe a [Round]
-    
+  }
+
+  it should "receive the dropped pins" in {
+    val round = Round()
+    round.throw(5)
   }
 }
